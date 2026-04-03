234971c348e1278de1136d4d25be3146e898db5e
doomzc <34374859+doomzc@users.noreply.github.com>
2026-04-04 01:02:55 +0800

    Update TgramTVApp.kt

--- app/src/main/java/com/momoko/tgramtvunlimited/TgramTVApp.kt
+++ app/src/main/java/com/momoko/tgramtvunlimited/TgramTVApp.kt
@@ -1,9 +1,12 @@
 package com.momoko.tgramtvunlimited
 
 import android.app.Application
+import com.momoko.tgramtvunlimited.data.api.TelegramApiClient
 
 class TgramTVApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        // Initialize TDLib
+        TelegramApiClient.getInstance(this).initialize()
     }
 }
